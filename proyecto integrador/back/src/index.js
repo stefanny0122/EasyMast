9a4d117ed9e51abfb2ca518951308e8b0b8ef33a
Stefanny  Tirado <102838610+stefanny0122@users.noreply.github.com>
2024-04-11 15:31:39 +0000

    Agregados cambios en archivos de servidores

---
+++ proyecto integrador/back/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const FacturaRouter = require ('../src/routers/Factura.routers.js'); 
+const MaterialRouter = require('../src/routers/MaterialDeConstruccion.routers.js');
+const NotaRouter = require ('../src/routers/NotaDeCredito.routers.js');
+const OrdenRouter = require ('../src/routers/OrdenDeCompra.routers.js');
+const PagoRouter = require ('../src/routers/Pago.routers.js');
+const UsuarioRouter = require ('../src/routers/Usuario.routers.js')
+const pool = require('../src/providers/conexion.js')
+
+app.use(cors('*'));
+app.use(express.json());
+
+app.use("/Factura", FacturaRouter);
+app.use("/MaterialDeConstruccion", MaterialRouter);
+app.use ("/NotaDeCredito", NotaRouter);
+app.use("/OrdenDeCompra", OrdenRouter);
+app.use("/Pago", PagoRouter);
+app.use ("/Usuario", UsuarioRouter);
+
