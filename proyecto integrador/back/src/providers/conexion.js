8efc1766710dee5451d80aeb13d856487c370ee0
Stefanny  Tirado <102838610+stefanny0122@users.noreply.github.com>
2024-04-24 03:20:47 +0000

    Agregados cambios en archivos de servidores

--- proyecto integrador/back/src/providers/conexion.js
+++ proyecto integrador/back/src/providers/conexion.js
@@ -12,10 +12,11 @@ const config = {
 async function connectToDatabase() {
   let connection;
   try {
+    console.log('Conectando a la base de datos...');
     connection = await mariadb.createConnection(config);
     console.log('Conexión establecida correctamente.');
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error.message);
+    console.error('Error al conectar a la base de datos:', error);
     throw error; // Relanzamos el error para que pueda ser manejado por el código que llama a esta función
   }
   return connection;
@@ -24,12 +25,13 @@ async function connectToDatabase() {
 async function main() {
   try {
     const connection = await connectToDatabase();
-    // Aquí puedes realizar operaciones con la conexión, si se estableció correctamente
     console.log('Operaciones con la base de datos...');
+    // Aquí puedes realizar operaciones con la conexión, si se estableció correctamente
     // Por ejemplo, puedes ejecutar consultas SQL aquí
     // await connection.query('SELECT * FROM tabla');
+    await connection.end(); // No olvides cerrar la conexión cuando hayas terminado
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error.message);
+    console.error('Error en la función principal:', error);
   }
 }
 
