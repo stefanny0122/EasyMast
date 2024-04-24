85cca5f7c62d2bdf6ece400436a472772a10acb6
Stefanny  Tirado <102838610+stefanny0122@users.noreply.github.com>
2024-04-24 03:18:07 +0000

    Agregados cambios en archivos de servidores

--- proyecto integrador/back/src/providers/conexion.js
+++ proyecto integrador/back/src/providers/conexion.js
@@ -2,13 +2,13 @@ const mariadb = require('mariadb');
 
 const config = {
   host: '192.168.3.8',
-  user: 'root',
-  password: '12345',
+  user: 'EasyMats',
+  password: '@EasyMats',
   database: 'BD_EASYMATS',
-  port:'3306'
+  port: 3306,
+  connectTimeout: 6000, // Aumenta el tiempo de espera a 5 segundos (5000 milisegundos)
 };
 
-// Función para conectar a la base de datos
 async function connectToDatabase() {
   let connection;
   try {
@@ -16,10 +16,24 @@ async function connectToDatabase() {
     console.log('Conexión establecida correctamente.');
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error.message);
+    throw error; // Relanzamos el error para que pueda ser manejado por el código que llama a esta función
   }
   return connection;
 }
 
-module.exports = {
-  connectToDatabase
-};
+async function main() {
+  try {
+    const connection = await connectToDatabase();
+    // Aquí puedes realizar operaciones con la conexión, si se estableció correctamente
+    console.log('Operaciones con la base de datos...');
+    // Por ejemplo, puedes ejecutar consultas SQL aquí
+    // await connection.query('SELECT * FROM tabla');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message);
+  }
+}
+
+main(); // Llama a la función main para iniciar la conexión
+
+
+
